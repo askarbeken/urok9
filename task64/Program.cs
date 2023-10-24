58ab6e47aafe743ca6655d85d712cf0659a89a5b
Askar <abekenov@mail.ru>
2023-10-25 02:27:58 +0600

    interim

---
+++ task64/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+void PrintNaturalNumbers(int N)
+{
+    if (N >= 1)
+    {
+        Console.WriteLine(N);
+        PrintNaturalNumbers(N - 1);
+    }
+}
+
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 1)
+{
+    Console.WriteLine("N должно быть натуральным числом или больше 1");
+}
+else
+{
+    Console.WriteLine($"Натуральные числа в промежутке от {N} до 1:");
+    PrintNaturalNumbers(N);
+}
+
+
+
+
+
---
+++ task66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (M > N)
+{
+    Console.WriteLine("M должно быть меньше или равно N");
+}
+else
+{
+    int result = CalculateSum(M, N);
+    Console.WriteLine($"Сумма натуральных чисел в промежутке от {M} до {N} равна {result}");
+}
+
+int CalculateSum(int M, int N)
+{
+    if (M > N)
+    {
+        return 0;
+    }
+    else
+    {
+        return M + CalculateSum(M + 1, N);
+    }
+}
